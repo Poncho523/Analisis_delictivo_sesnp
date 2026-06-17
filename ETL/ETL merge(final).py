abc6118e6fdd8e0a016c1d835ca97ce0fdd50e83
andypab <andrea.ba.pablo@gmail.com>
2026-06-16 18:08:29 -0600

    Refactor comments and clean up print outputs
    Updated comments for clarity and removed print statements.

--- ETL/ETL merge(final).py
+++ ETL/ETL merge(final).py
@@ -17,16 +17,11 @@ df_final = df_delitos.merge(
 df_final['POB_TOTAL'] = df_final['POB_TOTAL'].fillna(0).astype(int)
 
 # Calcular la tasa delictiva por cada 100,000 habitantes (Total Anual)
-# np.where evita el error matemático de dividir entre cero si algún municipio no tiene habitantes registrados
+# np.where evita que divida entre 0
 df_final['Tasa_Anual_100k'] = np.where(
     df_final['POB_TOTAL'] > 0, 
     (df_final['Total_Anual'] / df_final['POB_TOTAL']) * 100000, 
-    0
-).round(2)
+    0).round(2)
 
 nombre_archivo_salida = 'delitos_nivelados_2015.csv'
 df_final.to_csv(nombre_archivo_salida, index=False)
-
-print(f"¡ETL completado! Tu nuevo archivo '{nombre_archivo_salida}' está listo.")
-print(f"Total de filas originales de delitos: {len(df_delitos)}")
-print(f"Total de filas finales (debería ser el mismo número): {len(df_final)}")
